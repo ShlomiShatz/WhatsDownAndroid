44ba9d742a98f4674b1d94dd8f66a9bbfcb1ba67
Shlomo Shatz <shlomis132@gmail.com>
2023-06-09 02:01:48 +0300

    basic design

--- app/src/main/java/com/example/whatsdown/LoginActivity.java
+++ app/src/main/java/com/example/whatsdown/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.whatsdown;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView tvRegistration = findViewById(R.id.login_tvRegistration);
+        tvRegistration.setOnClickListener(v -> {
+            Intent i = new Intent(this, RegisterActivity.class);
+            startActivity(i);
+        });
     }
 }
--- app/src/main/java/com/example/whatsdown/RegisterActivity.java
+++ app/src/main/java/com/example/whatsdown/RegisterActivity.java
@@ -2,7 +2,9 @@ package com.example.whatsdown;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        TextView tvGotoLogin = findViewById(R.id.register_tvLogin);
+        tvGotoLogin.setOnClickListener(v -> {
+            Intent i = new Intent(this, LoginActivity.class);
+            startActivity(i);
+        });
     }
 }
