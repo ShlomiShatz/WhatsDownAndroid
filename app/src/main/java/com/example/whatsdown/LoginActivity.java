19b450fa1a7b399fac5a92810e241cfb6cd1a62c
Shlomo Shatz <shlomis132@gmail.com>
2023-06-09 19:19:23 +0300

    add verifications and design

--- app/src/main/java/com/example/whatsdown/LoginActivity.java
+++ app/src/main/java/com/example/whatsdown/LoginActivity.java
@@ -4,20 +4,58 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class LoginActivity extends AppCompatActivity {
+
+    TextInputLayout username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        username = findViewById(R.id.login_username);
+        password = findViewById(R.id.login_password);
+
         TextView tvRegistration = findViewById(R.id.login_tvRegistration);
         tvRegistration.setOnClickListener(v -> {
             Intent i = new Intent(this, RegisterActivity.class);
             startActivity(i);
         });
+    }
+
+    private boolean validateUsername() {
+        String value = username.getEditText().getText().toString();
+        if (value.isEmpty()) {
+            username.setError("Please insert username.");
+            return false;
+        } else {
+            username.setError(null);
+            username.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validatePassword() {
+        String value = password.getEditText().getText().toString();
+        if (value.isEmpty()) {
+            password.setError("Please insert password.");
+            return false;
+        } else {
+            password.setError(null);
+            password.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    public void onSubmitLogin(View view) {
+        boolean usr = validateUsername();
+        boolean pwd = validatePassword();
+        if (!usr || !pwd) return;
     }
 }
--- app/src/main/java/com/example/whatsdown/RegisterActivity.java
+++ app/src/main/java/com/example/whatsdown/RegisterActivity.java
@@ -6,17 +6,27 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class RegisterActivity extends AppCompatActivity {
 
     private final int GALLERY_REQ_CODE = 1000;
+    TextInputLayout username, password, verifyPassword, displayName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        username = findViewById(R.id.register_etUsername);
+        password = findViewById(R.id.register_etPassword);
+        verifyPassword = findViewById(R.id.register_etVerifyPassword);
+        displayName = findViewById(R.id.register_etDisplayName);
+
 
         TextView tvGotoLogin = findViewById(R.id.register_tvLogin);
         tvGotoLogin.setOnClickListener(v -> {
@@ -40,5 +50,74 @@ public class RegisterActivity extends AppCompatActivity {
             ImageView ivUploadedImage = findViewById(R.id.register_ivImageUploaded);
             ivUploadedImage.setImageURI(selectedImage);
         }
+    }
+
+
+
+    private boolean validateUsername() {
+        String regExp = "^[A-Za-z0-9]*$";
+        String value = username.getEditText().getText().toString();
+        if (value.isEmpty()) {
+            username.setError("Please insert username.");
+            return false;
+        } else if(!value.matches(regExp)) {
+            username.setError("Username must contain letters and numbers only.");
+            return false;
+        } else {
+            username.setError(null);
+            username.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validatePassword() {
+        String regExp = "^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[!@#$.]).{8,}$";
+        String value = password.getEditText().getText().toString();
+        if (value.isEmpty()) {
+            password.setError("Please insert password.");
+            return false;
+        } else if(!value.matches(regExp)) {
+            password.setError("Password must be 8 characters or more, contain lower case letters, upper case letters, numbers and at least one of these symbols: .!@#$");
+            return false;
+        } else {
+            password.setError(null);
+            password.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validatePasswordVerify() {
+        String value = verifyPassword.getEditText().getText().toString();
+        if (value.isEmpty()) {
+            verifyPassword.setError("Please insert the password again.");
+            return false;
+        } else if(!value.equals(password.getEditText().getText().toString())) {
+            verifyPassword.setError("Passwords must match.");
+            return false;
+        } else {
+            verifyPassword.setError(null);
+            verifyPassword.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateDisplayName() {
+        String value = displayName.getEditText().getText().toString();
+        if (value.isEmpty()) {
+            displayName.setError("Please insert display name.");
+            return false;
+        } else {
+            displayName.setError(null);
+            displayName.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    public void onSubmitRegister(View view) {
+        boolean usr = validateUsername();
+        boolean pwd = validatePassword();
+        boolean pwdvrf = validatePasswordVerify();
+        boolean dsp = validateDisplayName();
+        if (!usr || !pwd || !pwdvrf || !dsp) return;
     }
 }
