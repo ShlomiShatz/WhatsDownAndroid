9981b6ccfce208cfb5add00f16d82762598f8d84
Shlomo Shatz <shlomis132@gmail.com>
2023-06-09 12:39:57 +0300

    basic design

--- app/src/main/java/com/example/whatsdown/RegisterActivity.java
+++ app/src/main/java/com/example/whatsdown/RegisterActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -17,6 +20,14 @@ public class RegisterActivity extends AppCompatActivity {
         tvGotoLogin.setOnClickListener(v -> {
             Intent i = new Intent(this, LoginActivity.class);
             startActivity(i);
+        });
+
+        ImageView ivUploadedImage = findViewById(R.id.register_ivImageUploaded);
+        Button btnUploadImage = findViewById(R.id.register_btnUploadImage);
+
+        btnUploadImage.setOnClickListener(v -> {
+            Intent iUpload = new Intent(Intent.ACTION_PICK);
+            iUpload.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         });
     }
 }
